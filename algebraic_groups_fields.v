dc535b71aed915103e1c084270f6da0fb0dcc6e3
ekiciburak <ekcburak@hotmail.com>
2016-04-06 00:44:23 -0500

    abelian groups.

--- algebraic_groups_fields.v
+++ algebraic_groups_fields.v
@@ -1,6 +1,6 @@
 Require Import SetoidClass.
 
-Class group (G: Type) `(Setoid G) (opG: G -> G -> G) (zeroG: G) (invG: G -> G) :=
+Class group (G: Set) `(Setoid G) (opG: G -> G -> G) (zeroG: G) (invG: G -> G) :=
 {
   g_assoc    : forall {a b c}, opG (opG a b) c == opG a (opG b c);
   g_identity : forall {a}, opG zeroG a == a;
@@ -8,17 +8,18 @@ Class group (G: Type) `(Setoid G) (opG: G -> G -> G) (zeroG: G) (invG: G -> G) :
 }.
 Check group.
 
-Class abelian_group (G: Type) (sG: Setoid G) (opG: G -> G -> G) (zeroG: G) (invG: G -> G)
+Class abelian_group (G: Set) (sG: Setoid G) (opG: G -> G -> G) (zeroG: G) (invG: G -> G)
                     (gG: (@group G sG opG zeroG invG)) :=
 {
   g_comm    : forall {a b: G}, (opG a b) == (opG b a)
 }.
 Check abelian_group.
 
-Class group_hom (G H: Type) (h: G -> H) (sG: Setoid G) (opG: G -> G -> G) (zeroG: G) (invG: G -> G)
-                                        (sH: Setoid H) (opH: H -> H -> H) (zeroH: H) (invH: H -> H) 
-                                        `(@group G sG opG zeroG invG)
-                                        `(@group H sH opH zeroH invH)
+
+Class group_hom (G H: Set) (h: G -> H) (sG: Setoid G) (opG: G -> G -> G) (zeroG: G) (invG: G -> G)
+                                       (sH: Setoid H) (opH: H -> H -> H) (zeroH: H) (invH: H -> H) 
+                                       `(@group G sG opG zeroG invG)
+                                       `(@group H sH opH zeroH invH)
 :=
 {
   h1 : forall (a b: G), h (opG a b) == opH (h a) (h b);
@@ -26,34 +27,46 @@ Class group_hom (G H: Type) (h: G -> H) (sG: Setoid G) (opG: G -> G -> G) (zeroG
 }.
 Check group_hom.
 
-Definition kernel (G H: Type) (h: G -> H) (sG: Setoid G) (opG: G -> G -> G) (zeroG: G) (invG: G -> G)
-                                          (sH: Setoid H) (opH: H -> H -> H) (zeroH: H) (invH: H -> H)
+
+Definition kernel (G H: Set) (h: G -> H) (sG: Setoid G) (opG: G -> G -> G) (zeroG: G) (invG: G -> G)
+                                         (sH: Setoid H) (opH: H -> H -> H) (zeroH: H) (invH: H -> H)
                   (gG: (@group G sG opG zeroG invG))
                   (gH: (@group H sH opH zeroH invH))
                   `(@group_hom G H h sG opG zeroG invG sH opH zeroH invH gG gH) := { u: G | h u == zeroH }.
 Check kernel.
 
-Definition image (G H: Type) (h: G -> H) (sG: Setoid G) (opG: G -> G -> G) (zeroG: G) (invG: G -> G)
-                                         (sH: Setoid H) (opH: H -> H -> H) (zeroH: H) (invH: H -> H)
+Definition image (G H: Set) (h: G -> H) (sG: Setoid G) (opG: G -> G -> G) (zeroG: G) (invG: G -> G)
+                                        (sH: Setoid H) (opH: H -> H -> H) (zeroH: H) (invH: H -> H)
                  (u: G)
                  (gG: (@group G sG opG zeroG invG))
                  (gH: (@group H sH opH zeroH invH))
                  `(@group_hom G H h sG opG zeroG invG sH opH zeroH invH gG gH) := { hu: H | hu == h u}.
 Check image.
 
-
-Class field (F: Type) `(s: Setoid F) (op1 op2: F -> F -> F) (zero one: F) (inv1 inv2: F -> F)
-                      `(@group F s op1 zero inv1) :=
+Class field (F: Set) (s: Setoid F) (op1 op2: F -> F -> F) (zero one: F) (inv1 inv2: F -> F)
+                     (g1: @group F s op1 zero inv1) :=
 {
+
   f_assoc     : forall {a b c}, op2 (op2 a b) c == op2 a (op2 b c);
   f_identity  : forall {a}, op2 one a == a;
-  f_idsdiffer : ~ one == zero; 
-  f_inverse   : forall a: F, (a == zero) -> op2 (inv2 a) a == one -> False;
+  f_inverse   : forall a: F, a == zero -> op2 (inv2 a) a == one -> False;
+  f_idsdiffer : ~ one == zero;
   f_comm1     : forall {a b}, op1 a b == op1 b a;
   f_comm2     : forall {a b}, op2 a b == op2 b a;
   f_distrib   : forall {a b c}, (op2 a (op1 b c)) == op1 (op2 a b) (op2 a c)
 }.
 Check field.
+
+(** subgroups
+Definition subgroup (G: Set) (sG: Setoid G) (opG: G -> G -> G) (zeroG: G) (invG: G -> G)
+                             (gG: (@group G sG opG zeroG invG))
+                    (P: G -> Prop)
+                             {sSG: Setoid { u: G | P u }} {opSG: { u: G | P u } -> { u: G | P u } -> { u: G | P u }} {zeroSG: { u: G | P u }} 
+                             {invSG: { u: G | P u } -> { u: G | P u }} :=
+                    (@group { u: G | P u } sSG opSG zeroSG invSG).
+
+Check subgroup.
+**)
 
 Section int_instances.
 
@@ -76,21 +89,10 @@ Next Obligation. unfold zadd, zinv, zid. omega. Qed.
 
 Check group_integers.
 
-Definition zmult (n m: Z) := n * m.
-Definition zid2 := 1.
-Definition zinv2 (n: Z) := 1 / n.
+Program Instance abelian_group_integers: (@abelian_group Z (Zeq_setoid) zadd zid zinv group_integers).
+Obligation 1. unfold zadd. omega. Qed.
 
-(** < Z, +, *, 0, 1, ^{-1+}, ^{-1*} > as a field instance **)
-Program Instance field_integers: `(@field Z (Zeq_setoid) _ zmult _ zid2 _ zinv2 group_integers).
-Obligation 1. unfold zmult. apply Zmult_assoc_reverse. Qed.
-Next Obligation. induction a; reflexivity. Qed.
-(** If it fails here that means that obligations 3 and 4 cannot be proven automatically.
-To get these proofs, please make sure that csdp is installed and added to the PATH **)
-Next Obligation. unfold zadd. omega. Qed.
-Next Obligation. unfold zmult. apply Zmult_comm. Qed.
-Next Obligation. unfold zadd,zmult. apply Zmult_plus_distr_r. Qed.
-Check field_integers.
-End int_instances.
+Check abelian_group_integers.
 
 Section rat_instances.
 
@@ -119,8 +121,8 @@ Definition qinv2 (n: Q) := 1 / n.
 
 (** < Q, +, *, 0, 1, ^{-1+}, ^{-1*} > as a field instance **)
 Program Instance field_rationals: `(@field Q (Qeq_setoid) _ qmult _ qid2 _ qinv2 group_rationals).
-Obligation 1. unfold qmult. symmetry. apply Qmult_assoc. Qed.
-Next Obligation. unfold qmult, qid2. apply Qmult_1_l. Qed.
+Obligation 1. unfold qmult. symmetry. apply Qmult_assoc. Qed. 
+Next Obligation. unfold qmult, qid2. apply Qmult_1_l. Qed. 
 Next Obligation. unfold qmult, qinv2, qid2, qid in *. revert H0. rewrite H. psatz Q 1. Qed.
 Next Obligation. unfold qadd. apply Qplus_comm. Qed.
 Next Obligation. unfold qmult. apply Qmult_comm. Qed.
@@ -129,7 +131,6 @@ Next Obligation. unfold qadd, qmult. apply Qmult_plus_distr_r. Qed.
 Check field_rationals.
 
 End rat_instances.
-
 
 Section real_instances.
 
@@ -143,7 +144,7 @@ Definition rid := 0.
 Definition rinv (n: R) := -n.
 
 Program Instance Req_setoid : Setoid R :=
-  { equiv := eq ; setoid_equiv :=  eq_equivalence}.
+  { equiv := (eq(A:=R)) ; setoid_equiv := eq_equivalence}.
 
 (** < R, +, 0, ^{-1+} > as a group instance **)
 Program Instance group_reals: (@group R (Req_setoid) radd rid rinv).
@@ -159,15 +160,15 @@ Definition rid2 := 1%R.
 Definition rinv2 (n: R) := 1 / n.
 
 (** < R, +, *, 0, 1, ^{-1+}, ^{-1*} > as a field instance **)
-(** Obligation 4 has currently no witness **)
-Program Instance field_reals: `(@field R (Req_setoid) _ rmult _ rid2 _ rinv2 group_reals).
+(** Obligation 3 has currently no witness **)
+Program Instance field_reals: `(@field R _ _ rmult _ rid2 _ rinv2 group_reals).
 Obligation 1. unfold rmult. apply Rmult_assoc. Qed.
-Obligation 2. unfold rmult, rid2. apply Rmult_1_l. Qed.
-Obligation 3. unfold rid, rid2. apply R1_neq_R0. Qed.
-(* Obligation 4. unfold rmult, rinv2, rid2, rid in *. intros. revert H0. psatz R. admit. Qed. *)
-Obligation 5. unfold radd. apply Rplus_comm. Qed.
-Obligation 6. unfold rmult. apply Rmult_comm. Qed.
-Obligation 7. unfold radd, rmult. apply  Rmult_plus_distr_l. Qed.
+Next Obligation. unfold rmult, rid2. apply Rmult_1_l. Qed.
+Next Obligation. unfold rmult, rinv2, rid2, rid in *. psatz R 1. intros. contradict H0. intros. admit. (**mind the admit here**) Qed. 
+Next Obligation. unfold rid, rid2. apply R1_neq_R0. Qed.
+Next Obligation. unfold radd. apply Rplus_comm. Qed.
+Next Obligation. unfold rmult. apply Rmult_comm. Qed.
+Next Obligation. unfold radd, rmult. apply  Rmult_plus_distr_l. Qed.
 
-(* Check field_reals.
-End real_instances. *)
+Check field_reals.
+End real_instances.
